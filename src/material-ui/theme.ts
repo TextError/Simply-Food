97f08f630f0e0293fe1a27617902c282ec96b98d
TextError <savinraul1983@yahoo.com>
2021-05-11 16:34:01 +0100

    Added primary/secondary and white colors

--- src/material-ui/theme.ts
+++ src/material-ui/theme.ts
@@ -1,9 +1,21 @@
 import { createMuiTheme } from "@material-ui/core";
-import { green } from "@material-ui/core/colors";
+import { lightBlue, grey } from "@material-ui/core/colors";
 
 export const theme = {
   primary: {
-    main: green[500],
+    light: lightBlue[100],
+    main: lightBlue[300],
+    dark: lightBlue[600],
+  },
+  secondary: {
+    light: '#C6693F',
+    main: '#F7834F',
+    dark: 'F99C72'
+  },
+  white: {
+    light: grey[100],
+    main: grey[300],
+    dark: grey[600],
   }
 }
 
--- src/styled-components/Base.ts
+++ src/styled-components/Base.ts
@@ -9,7 +9,21 @@ const Base = createGlobalStyle`
   };
   
   :root{
+    --lightPrimary: ${theme.primary.light};
     --primary: ${theme.primary.main};
+    --darkPrimary: ${theme.primary.dark};
+    --lightSecondary: ${theme.secondary.light};
+    --secondary: ${theme.secondary.main};
+    --darkSecondary: ${theme.secondary.dark};
+    --lightWhite: ${theme.white.light};
+    --white: ${theme.white.main};
+    --darkWhite: ${theme.white.dark};
+    --navbar: 64px;
+    --footer: 188px;
+    --transition: .3s;
+    --transitionLinear: all .3s linear;
+    --minHeight: calc(100vh - var(--navbar) - var(--footer));
+    --padding: 24px;
   };
 `
 
