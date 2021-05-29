c445ac4b294a2a5dd3a93cb79236952b5e0b03d0
TextError <savinraul1983@yahoo.com>
2021-05-29 18:22:52 +0100

    [Contact] form input

---
+++ src/components/contact/wrapper/common/Input.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import isEmpty from "./isEmpty";
+import { TextField } from "@material-ui/core";
+
+const Input: React.FC<IProps> = ({ label, name, value, onChange, onFocus, error, helperText, type }) => (
+  <TextField
+    label={isEmpty(error) ? label : error}
+    name={name}
+    value={value}
+    onChange={e => onChange(e)}
+    onFocus={() => onFocus({ name })}
+    error={!isEmpty(error)}
+    helperText={helperText}
+    type={type}
+    focused={error ? true : undefined}
+    id={`outlined-${label}`}
+    variant="outlined"
+  />
+);
+
+interface IProps {
+  label: string
+  name: string
+  value: string
+  onChange: (e:React.InputHTMLAttributes<HTMLInputElement> & { target: { name: string, value: string }}) => void
+  onFocus: ({ name }: { name: string }) => void
+  error: string
+  helperText?: string
+  type: string
+};
+
+export default Input;
--- src/components/contact/wrapper/form/Form.tsx
+++ src/components/contact/wrapper/form/Form.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import isEmpty from "../common/isEmpty";
+import { IIndexable, IOnChange } from "../common/interface";
+
+import { Paper } from '@material-ui/core';
+import Input from '../common/Input';
 
 const Form: React.FC = () => {
+  const [state, setState] = useState({ name: '', email: ''});
+  const [error, setError] = useState({ name: '', email: '' });
+
+  const { name, email } = state;
+
+  const onChange = ({ target: { name, value }}: IOnChange): void => setState({ ...state, [name]: value });
+  
+  const onFocus = ({ name }: { name: string }): false | void => {
+    const obj: IIndexable = { ...error };
+    !isEmpty(obj[name]) && setError({ ...error, [name]: '' });
+  };
+  
   return (
-    <>
-      form
-    </>
+    <Paper>
+      <Input
+        label={'Name *'}
+        name='name'
+        value={name}
+        onChange={onChange}
+        onFocus={onFocus}
+        error={error.name}
+        type='name'
+      />
+      <Input
+        label={'Email *'}
+        name='email'
+        value={email}
+        onChange={onChange}
+        onFocus={onFocus}
+        error={error.email}
+        type='email'
+      />
+    </Paper>
   )
 }
 
