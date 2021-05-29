c1961b493a0f3a4f52373c1e3d4a72671513e983
TextError <savinraul1983@yahoo.com>
2021-05-29 19:06:06 +0100

    [Contact] TextArea component

--- src/components/contact/wrapper/form/Form.tsx
+++ src/components/contact/wrapper/form/Form.tsx
@@ -4,12 +4,13 @@ import { IIndexable, IOnChange } from "../common/interface";
 
 import { Grid } from '@material-ui/core';
 import Input from '../common/Input';
+import TextArea from '../common/TextArea';
 
 const Form: React.FC = () => {
-  const [state, setState] = useState({ name: '', email: ''});
-  const [error, setError] = useState({ name: '', email: '' });
+  const [state, setState] = useState({ name: '', email: '', message: ''});
+  const [error, setError] = useState({ name: '', email: '', message: '' });
 
-  const { name, email } = state;
+  const { name, email, message } = state;
 
   const onChange = ({ target: { name, value }}: IOnChange): void => setState({ ...state, [name]: value });
   
@@ -24,7 +25,7 @@ const Form: React.FC = () => {
   };
   
   return (
-    <Grid item sm={6} xs={12} component='form' onSubmit={onSubmit} noValidate >
+    <Grid item sm={6} xs={12} component='form' onSubmit={onSubmit} noValidate>
       <Input
         label={'Name *'}
         name='name'
@@ -43,8 +44,17 @@ const Form: React.FC = () => {
         error={error.email}
         type='email'
       />
+      <TextArea 
+        label={'Message *'}
+        name='message'
+        value={message}
+        onChange={onChange}
+        onFocus={onFocus}
+        error={error.message}
+        type='text'
+      />
     </Grid>
-  )
-}
+  );
+};
 
 export default Form;
