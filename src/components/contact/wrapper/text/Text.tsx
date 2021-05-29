cb8b17b41d5483a2a10d4169c2cc22621910a14a
TextError <savinraul1983@yahoo.com>
2021-05-29 17:36:12 +0100

    [Contact] Text component

--- src/components/contact/wrapper/text/Text.tsx
+++ src/components/contact/wrapper/text/Text.tsx
@@ -1,11 +1,13 @@
+import { Typography } from '@material-ui/core';
 import React from 'react';
 
-const Text: React.FC = () => {
-  return (
-    <div>
-      text
-    </div>
-  )
-}
+const Text: React.FC = () => (
+  <section>
+    <Typography variant='h1'>Want To Get In Touch?</Typography>
+    <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio deserunt autem ipsum ipsa laudantium quaerat.</Typography>
+    <Typography>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</Typography>
+    <Typography>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum debitis expedita cum labore, tempore deleniti eveniet. Quasi saepe nisi veritatis!</Typography>
+  </section>
+)
 
 export default Text;
